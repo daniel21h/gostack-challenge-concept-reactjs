2cf8c4308e6eace63981a3e8c7f00ebec85ab6f1
daniel21h <hesseldaniel21@gmail.com>
2020-04-15 11:12:47 -0300

    Entrega do desafio

--- src/App.js
+++ src/App.js
@@ -25,7 +25,11 @@ function App() {
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+
+    setRepositories(repositories.filter(
+      repository => repository.id !== id
+    ))
   }
 
   return (
@@ -35,7 +39,7 @@ function App() {
           <li key={repository.id}>
             {repository.title}
 
-            <button onClick={() => handleRemoveRepository(1)}>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
               Remover
             </button>
           </li>
